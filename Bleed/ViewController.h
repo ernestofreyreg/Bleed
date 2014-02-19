c66f4d28939b7620bba6680c3f5b0734cab357ec
Ernesto <ernestfreyre@gmail.com>
2014-02-19 17:43:33 -0500

    Initial Commit

---
+++ Bleed/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Bleed
+//
+//  Created by Ernesto Freyre on 19/02/14.
+//  Copyright (c) 2014 Ernesto Freyre G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Bleed/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Bleed
+//
+//  Created by Ernesto Freyre on 19/02/14.
+//  Copyright (c) 2014 Ernesto Freyre G. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
